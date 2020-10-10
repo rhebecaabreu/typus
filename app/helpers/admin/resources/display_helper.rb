fad8d1d7dc53e053bf9598e7fceb6de9ea1d02de
rhebecaabreu <rhebecaabreu@gmail.com>
2020-10-09 22:29:37 -0400

    [FIX] Error

--- app/helpers/admin/resources/display_helper.rb
+++ app/helpers/admin/resources/display_helper.rb
@@ -8,6 +8,7 @@ module Admin::Resources::DisplayHelper
     fields.map do |attribute, type|
       condition = (type == :boolean) || item.respond_to?(attribute)
       next unless condition
+
       [@resource.human_attribute_name(attribute), send("display_#{type}", item, attribute)]
     end.compact
   end
@@ -16,7 +17,8 @@ module Admin::Resources::DisplayHelper
     String.new.tap do |html|
       @resource.typus_defaults_for(:relationships).each do |relationship|
         association = @resource.reflect_on_association(relationship.to_sym)
-        macro, klass = association.macro, association.class_name.constantize
+        macro = association.macro
+        klass = association.class_name.constantize
         if [:has_many, :has_one].include?(macro) && admin_user.can?('read', klass)
           html << send("typus_form_#{macro}", relationship)
         end
